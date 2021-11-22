c5ac210176ab896f6d2164965926eb9ebbbd00b4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-11-22 08:08:43 +0530

    Update roboshop/cart/

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,5 +1,9 @@
 module "sample" {
-  source        = "git::https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/terraform-mutable//app-module"
-  INSTANCE_TYPE = var.INSTANCE_TYPE
+  source              = "git::https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/terraform-mutable//app-module"
+  INSTANCE_TYPE       = var.INSTANCE_TYPE
+  ENV                 = var.ENV
+  SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
+
 }
 
